24ebc1388dda41a89c981595bf2629801e7abb89
whybazinga <aom9@mail.ru>
2018-03-26 18:24:29 +0300

    minor code cleaning and bug fixes

--- src/signin.js
+++ src/signin.js
@@ -1,8 +1,8 @@
-const signInPage = new function () {
-    this.page = () => {
-        let main = document.createElement('main');
-        main.className = 'login';
-        main.innerHTML = `
+const signInPage = new function() {
+  this.page = () => {
+    let main = document.createElement("main");
+    main.className = "login";
+    main.innerHTML = `
             <div class="welcome-header">
                 <h2>Sign in and...</h2>
                 <h1>Carpe Diem</h1>
@@ -20,40 +20,47 @@ const signInPage = new function () {
                 <h3>Invalid input</h3>
             </form>
             `;
-        return main;
-    }
+    return main;
+  };
 
-    this.init = () => {
-        document.forms['login-form'].onsubmit = (e) => {
-            e.preventDefault();
-            const user = {
-                login: e.currentTarget[0].value,
-                password: e.currentTarget[1].value,
-            }
-            // if (users.find((element) => {
-            //     return element.username === user.login && element.password === user.password;
-            // })) {
-            if (user.login === admin.username && admin.password === user.password) {
-                window.goToPage('thread');
-                window.changeUser(user.login);
-                domF.addPhotoPosts();
-            } else {
-                alert('Invalid input');
-            }
-        }
-        document.querySelector('.welcome-header>h1').onclick = () => { goToPage('thread', true) };
-    }
+  this.init = () => {
+    document.forms["login-form"].onsubmit = e => {
+      e.preventDefault();
+      const user = {
+        login: e.currentTarget[0].value,
+        password: e.currentTarget[1].value
+      };
+      if (
+        users.find(element => {
+          return (
+            element.username === user.login &&
+            element.password === user.password
+          );
+        })
+      ) {
+        window.goToPage("thread");
+        window.changeUser(user.login);
+      } else {
+        alert("Invalid input");
+      }
+    };
+    document.querySelector(".welcome-header>h1").onclick = () => {
+      goToPage("thread", true);
+    };
+  };
 
-    this.load = () => {
-        document.querySelector('header').style.display = 'none';
-        document.querySelector('#root').insertBefore(this.page(), document.querySelector('footer'));
-        this.init();
-    }
+  this.load = () => {
+    document.querySelector("header").style.display = "none";
+    document
+      .querySelector("#root")
+      .insertBefore(this.page(), document.querySelector("footer"));
+    this.init();
+  };
 
-    this.unload = () => {
-        document.querySelector('header').style.display = '';
-        document.forms['login-form'].onsubmit = null;
-    }
+  this.unload = () => {
+    document.querySelector("header").style.display = "";
+    document.forms["login-form"].onsubmit = null;
+  };
 
-    this.changeUser = () => { }
-};
+  this.changeUser = () => {};
+}();
--- src/storage.js
+++ src/storage.js
@@ -1,65 +1,86 @@
-const admin = {
-    username: 'Alex',
-    password: '123',
-    posts: ['1', '2', '3'],
-};
+const ls = (function() {
+  return {
+    init: () => {},
+
+    getUsers: () => {
+      localStorage.getItem(0);
+    },
+    getPosts: () => {
+      localStorage.getItem(1);
+    }
+  };
+})();
+
+const users = [
+  {
+    username: "Alex",
+    password: "123"
+    // posts: ["1", "2", "3"]
+  },
+  {
+    username: "Gena",
+    password: "0"
+  }
+];
 
 const photoPosts = [
-    {
-        id: '1',
-        description: 'в Пхёнчхане!!!',
-        createdAt: new Date(2014, 1, 20),
-        author: 'Alex',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
-        hashTags: ['#sport'],
-        likes: [],
-    },
-    {
-        id: '2',
-        description: 'sample text',
-        createdAt: new Date('2014-02-23T23:00:00'),
-        author: 'Alex',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
-        hashTags: ['#sport'],
-        likes: [],
-    },
-    {
-        id: '3',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        createdAt: new Date('2014-02-23T23:00:00'),
-        author: 'Alex',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
-        hashTags: ['#sport'],
-        likes: [],
-    },
-    {
-        id: '4',
-        description: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        createdAt: new Date('2015-02-23T23:00:00'),
-        author: 'Xela',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
-        hashTags: ['#sport'],
-        likes: [],
-    },
-    {
-        id: '5',
-        description: 'Its so boring to write this objects',
-        createdAt: new Date('2016-02-23T23:00:00'),
-        author: 'Gena',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
-        hashTags: ['#sport'],
-        likes: [],
-    },
-    {
-        id: '6',
-        description: 'Gena is really cool',
-        createdAt: new Date('2015-02-23T23:00:00'),
-        author: 'Gena',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg',
-        hashTags: ['#sport'],
-        likes: [],
-    },
-]
+  {
+    id: "1",
+    description: "в Пхёнчхане!!!",
+    createdAt: new Date(2014, 1, 20),
+    author: "Alex",
+    photoLink: "http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg",
+    hashTags: ["#sport"],
+    likes: []
+  },
+  {
+    id: "2",
+    description: "sample text",
+    createdAt: new Date("2014-02-23T23:00:00"),
+    author: "Alex",
+    photoLink: "http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg",
+    hashTags: ["#sport"],
+    likes: []
+  },
+  {
+    id: "3",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    createdAt: new Date("2014-02-23T23:00:00"),
+    author: "Alex",
+    photoLink: "http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg",
+    hashTags: ["#sport"],
+    likes: []
+  },
+  {
+    id: "4",
+    description:
+      "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    createdAt: new Date("2015-02-23T23:00:00"),
+    author: "Xela",
+    photoLink: "http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg",
+    hashTags: ["#sport"],
+    likes: []
+  },
+  {
+    id: "5",
+    description: "Its so boring to write this objects",
+    createdAt: new Date("2016-02-23T23:00:00"),
+    author: "Gena",
+    photoLink: "http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg",
+    hashTags: ["#sport"],
+    likes: []
+  },
+  {
+    id: "6",
+    description: "Gena is really cool",
+    createdAt: new Date("2015-02-23T23:00:00"),
+    author: "Gena",
+    photoLink: "http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg",
+    hashTags: ["#sport"],
+    likes: []
+  }
+];
 
 // function getCurrentUser() {
 //     return JSON.parse(sessionStorage.getItem('currentUser'));
